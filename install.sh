5d592a503b0ec3db93fd80ebf442bf7363c6b41f
KeTian <ririhedou@gmail.com>
2018-06-04 16:02:15 -0400

    add demo

---
+++ install.sh
@@ -0,0 +1,28 @@
+# install chrome debian
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+
+# for the centos, try yum
+
+
+### get your chrome version
+apt list --installed | grep chrome
+
+
+uname -a
+# Linux XXXX 3.13.0-132-generic #181-Ubuntu SMP Wed Sep 13 13:25:03 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux
+
+
+google-chrome --version
+# Google Chrome 62.0.3202.89
+
+
+npm -v
+#5.6.0
+
+nodejs -v
+# v8.5.0
+
+echo "install puppeteer for Chrome headless"
+
+npm install puppeteer
